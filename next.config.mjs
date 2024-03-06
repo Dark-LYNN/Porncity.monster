3ada3cd4a623fdb9765b0906517e7533cf57c6e8
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-03-06 15:34:44 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['cdn.discordapp.com', 'cdn.lynnux.xyz'],
+    },
+};
 
 export default nextConfig;
