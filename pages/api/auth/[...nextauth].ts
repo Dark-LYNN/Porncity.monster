15e70cc54c08b035b05cbd3c45f2828cb868173c
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-05-04 20:44:28 +0200

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,6 @@
 import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
 import DiscordProvider from 'next-auth/providers/discord';
-import { saveUser } from './userService';
 
 const authHandler: NextApiHandler = NextAuth({
   providers: [
@@ -13,17 +12,9 @@ const authHandler: NextApiHandler = NextAuth({
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async signIn({ user, account, profile, email }) {
-      if (user.email) {  // Check if email exists to qualify as a new user check
-        const isNewUser = await saveUser(user); // Adapt your saveUser to return if new user
-        // Continue based on isNewUser if needed
-      }
-      return true;
-    },
     async session({ session, token }) {
       if (session.user) {
-        session.user.id = token.sub;  // Assuredly adding properties after existence check
-        session.user.email = token.email;
+        session.user.id = token.sub;
       }
       return session;
     }
