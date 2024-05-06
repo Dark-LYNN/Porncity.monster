b0a8e6a98f8e75f4575c6e32695b356502858c02
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-05-06 17:42:01 +0200

    ab

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -5,7 +5,6 @@ module.exports = {
   i18n: {
     defaultLocale: 'en-US',
     locales: ['en-US', 'nl-NL', 'tr-TR'],
-    
   },
   localePath: path.resolve('./public/locales'),
   localeExtension: 'yaml', // YAML extension  
