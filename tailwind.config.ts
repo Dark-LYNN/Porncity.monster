5abe8815abffa40e1871cee1fed26a7f45169841
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-05-06 18:56:40 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,17 @@
 // tailwind.config.ts
-
-import { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
         primary: '#B70640',
@@ -31,10 +29,9 @@ const config: Config = {
         white: '#FFFFFF',
         black: '#000000',
         background: '#333333',
-        
-        link: '#ED0770'
-      }
 
+        link: '#ED0770',
+      },
     },
   },
   plugins: [],
