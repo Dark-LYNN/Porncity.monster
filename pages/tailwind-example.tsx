f41c1125d7c129a0120fe0d14796c10f113f6a28
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-05-06 19:26:20 +0200

    translation

--- pages/tailwind-example.tsx
+++ pages/tailwind-example.tsx
@@ -4,6 +4,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
 import styles from '@/styles/404.module.css'
 import React from 'react';
+import LocalizedLink from '@/components/interface/LocalizedLink';
 
 const TailwindExamplePage = () => {
     const { t } = useTranslation('common');
@@ -13,8 +14,8 @@ const TailwindExamplePage = () => {
         <div className={`${styles.transfer} relative`} >
             <div className="bg-blue-500 text-white p-4 mt-28 mb-32 w-3/4 text-center center">
                 <h1 className="text-3xl font-bold">{t('Tailwind CSS Example')}</h1>
-                <p className="text-lg">{t('This is a to see if Tailwind CSS is working.')}</p>
-                <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 mt-4 rounded">{t('Click Me')}</button>
+                <p className=" pb-4 text-lg">{t('This is a to see if Tailwind CSS is working')}</p>
+                <LocalizedLink  className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded" href='/'>{t('Click Me')}</LocalizedLink>
             </div>
         </div>
     </>
